2e2853b99a62e9146a1340f4d7d009bc1fb0f078
Will <contatogwill@gmail.com>
2025-11-03 11:33:40 -0300

    Adicionando sistema de nome, envio de email e escondendo processo

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,26 +1,71 @@
+import smtplib
+import emoji
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 from time import sleep
 import os
 from dotenv import load_dotenv
 
+
+opcoes = webdriver.ChromeOptions()
+opcoes.add_argument('--headless=new')
 load_dotenv()
 
 email = os.getenv("UCB_EMAIL")
 password = os.getenv("UCB_PASS")
 
-
 MEU_PERGAMUM = 'https://ucb.pergamum.com.br/meupergamum'
 
 URL = 'https://ucb.pergamum.com.br/login?redirect=%2F'
 
+def nome() -> str:
+    usuario = os.getenv("UCB_EMAIL").split("@")[0]
+
+    partes = usuario.split(".")
+
+    nome = " ".join(p.capitalize() for p in partes)
+    return nome
+
+def sendemail(msg):
+
+    smtp_server = "smtp.office365.com"
+    port = 587
+    remetente = os.getenv("UCB_EMAIL")
+    senha = os.getenv("UCB_PASS")
+    hoje = datetime.now().strftime("%d/%m/%Y")
+
+    email_envio = MIMEMultipart()
+    email_envio["From"] = os.getenv("UCB_EMAIL")
+    email_envio["To"] = os.getenv("UCB_EMAIL")
+    email_envio["Subject"] = emoji.emojize(":books: Renovação Livros - {} :books:".format(hoje))
+
+    corpo = f"""  
+    <p>Olá, {nome()}! 😄</p>
+
+    <p>Os seus livros {msg} </p>
+    <p>At.te, BoBot 🤖 | {hoje}! </p>
+    """
+    email_envio.attach(MIMEText(corpo, "html"))
+    try:
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls()
+            server.login(remetente, senha)
+            server.send_message(email_envio)
+        print("E-mail enviado com sucesso!")
+    except Exception as e:
+        print(f"Erro ao enviar e-mail: {e}")
+
+def renovados(foram):
+    if foram:
+        return "foram renovados hoje! 😁"
+    else:
+        return "não foram renovados hoje! 😔"
 
 def logado(web):
     html = web.page_source
@@ -31,7 +76,7 @@ def pendente(web):
     return "Títulos pendentes" in livros
 
 
-web = webdriver.Chrome()
+web = webdriver.Chrome(options = opcoes)
 web.maximize_window()
 web.get(URL)
 
@@ -46,10 +91,13 @@ except:
 
 sleep(3)
 
+web.find_element(By.XPATH, '//*[@id="pergamum"]/div[2]/div/div[1]/div/div'
+                               '[1]/div/div[2]/div[2]/form/div[1]/div[2]/div').click()
+if logado(web):
+    print("Logado!")
+
 if not logado(web):
 
-    web.find_element(By.XPATH, '//*[@id="pergamum"]/div[2]/div/div[1]/div/div'
-                               '[1]/div/div[2]/div[2]/form/div[1]/div[2]/div').click()
     WebDriverWait(web,30).until(EC.element_to_be_clickable((By.ID, 'i0116')))
     web.find_element(By.XPATH, '//*[@id="i0116"]').click()
     web.find_element(By.XPATH, '//*[@id="i0116"]').send_keys(email)
@@ -78,6 +126,7 @@ if pendente(web):
     print("Existem títulos pendentes!")
 
     botoes = web.find_elements(By.XPATH, "//button[@title='Renovar']")
+    foram = False
 
     for botao in botoes:
         try:
@@ -86,17 +135,23 @@ if pendente(web):
                 EC.presence_of_element_located((By.CSS_SELECTOR, '[role="alert"]'))
             )
             mensagem = web.find_element(By.CSS_SELECTOR, '[role="alert"]').text
-            if "Livro renovado!" in mensagem.lower():
+            if "Renovado com sucesso!" in mensagem.lower():
                 print("Livro renovado!")
+                foram = True
+
             else:
-                print("Livro não pode ser renvoado hoje!")
+                print("Livro não pôde ser renvoado hoje!")
         except Exception as e:
             print(f"Erro ao tentar renovar: {e}")
+            foram = False
+    msg = renovados(foram)
+    sendemail(msg)
 
 else:
     print("Nenhum Título pendente!")
+    sendemail("Não foram renovados, pois não há títulos pendentes!")
 
+sleep(5)
+print("Processo finalizado!")
+web.quit()
 
-
-
-sleep(9999)
